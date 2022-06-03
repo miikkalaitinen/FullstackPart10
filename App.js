81a700bd6a2c2de4230820355feecb0f6d0013de
miikkalaitinen <38249083+miikkalaitinen@users.noreply.github.com>
2022-06-03 11:13:12 +0300

    Added eslint rules and emulator

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
+import { useState } from 'react/cjs/react.development'
 
 export default function App() {
+  const [visible, setVisible] = useState(true)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>
+        Open up App.js to start working on your app!
+      </Text>
+      <Image
+        style={{ display: visible ? 'flex' : 'none' }}
+        source={require('./assets/favicon.png')}
+      />
+      <Text onPress={() => setVisible(!visible)}>
+        {' '}
+        Click to {visible ? 'hide' : 'show'} logo{' '}
+      </Text>
       <StatusBar style="auto" />
     </View>
   )
@@ -16,5 +29,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    margin: 10,
   },
 })
